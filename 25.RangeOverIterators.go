15ad4d0bf2110acea0d6b1225965ea04b68a9b95
Qiao <noneplus@sina.com>
2025-04-24 17:15:49 +0800

    2025-04-24
    - Iterator和for的区别，
      数据处理客户端的解耦与否，slices.value()方法内部使用迭代器实现
    
    # Go语言默认对迭代器的支持包括：
    
    1. for...range 循环实际上是一种语法糖，它在底层使用了迭代器模式
    
    2. iter 包 (Go 1.23) 支持自定义迭代器
    
    3. maps 和 slices 包 (Go 1.21)
    
    maps.Values(m map[K]V) []V: 返回一个包含 Map m 中所有值的切片。
    slices.Contains(s []T, v T) bool: 判断切片 s 是否包含元素 v。
    
    4. bufio.Scanner
    
    bufio.Scanner 用于从 io.Reader 中读取数据，例如从文件中逐行读取数据。bufio.Scanner 提供了 Scan()、Text() 和 Err() 等方法，用于迭代读取数据。
    
    5. 第三方库
    
    除了标准库之外，还有一些第三方库提供了更丰富的迭代器工具，例如：
    
    github.com/jinzhu/gorm: GORM 是一个流行的 ORM 库，它提供了用于查询数据库的迭代器。
    github.com/xitongsys/parquet-go: parquet-go 是一个用于处理 Parquet 文件的库，它提供了用于读取 Parquet 文件的迭代器。

--- 25.RangeOverIterators.go
+++ 25.RangeOverIterators.go
@@ -27,7 +27,6 @@ func (lst *List[T]) Push(v T) {
 
 func (lst *List[T]) All() iter.Seq[T] {
 	return func(yield func(T) bool) {
-
 		for e := lst.head; e != nil; e = e.next {
 			if !yield(e.val) {
 				return
@@ -39,7 +38,6 @@ func (lst *List[T]) All() iter.Seq[T] {
 func genFib() iter.Seq[int] {
 	return func(yield func(int) bool) {
 		a, b := 1, 1
-
 		for {
 			if !yield(a) {
 				return
@@ -54,7 +52,6 @@ func main() {
 	lst.Push(10)
 	lst.Push(13)
 	lst.Push(23)
-
 	for e := range lst.All() {
 		fmt.Println(e)
 	}
@@ -63,10 +60,10 @@ func main() {
 	fmt.Println("all:", all)
 
 	for n := range genFib() {
-
 		if n >= 10 {
 			break
 		}
 		fmt.Println(n)
+
 	}
 }
